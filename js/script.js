99acbe331ef059e25867b7df1c0c4c7a4b9a45c1
Mikle Seregin <mikhail-seregin@yandex.ru>
2022-06-30 10:24:23 +0300

    add mail send

--- js/script.js
+++ js/script.js
@@ -83,6 +83,7 @@ class Mmenu{
 class Call{
     constructor(selector) {
         this.call = document.querySelector(selector);
+        this.form = this.call.querySelector('.call__form');
 
         this.init();
     }
@@ -92,6 +93,29 @@ class Call{
             if (!e.target.closest('.call__form') || e.target.closest('.call__close')){
                 this.close(e);
             }
+        });
+
+        this.form.addEventListener('submit', (e)=>{
+            e.preventDefault();
+
+            let data = $(this.form).serialize();
+
+            $.ajax({
+                dataType: "json",
+                type: "POST",
+                url: '/php/ajax.php',
+                data: data,
+                success: (result)=> {
+                    if (result.status) {
+                        this.form.classList.add('ok');
+                    } else {
+                        alert('Что-то пошло не так, попробуйте еще раз!!!');
+                    }
+                },
+                error: function (result) {
+                    alert('Что-то пошло не так, попробуйте еще раз!!!');
+                }
+            });
         });
     }
 
--- php/ajax.php
+++ php/ajax.php
@@ -1,20 +1,64 @@
 <?php
 header('Content-Type: application/json');
 
-$data = $_POST;
-$action = $data['action'];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-switch ($action) {
-    case 'call':
-        echo json_encode(array(
-            'status' => true
-        ));
-        exit();
-        break;
-    default:
-        echo json_encode(array(
-            'status' => false,
-        ));
-        exit();
-        break;
+require $_SERVER['DOCUMENT_ROOT'] . '/php/PHPMailer/src/Exception.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/php/PHPMailer/src/PHPMailer.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/php/PHPMailer/src/SMTP.php';
+
+function sendMail()
+{
+
+    ob_start();
+    ?>
+    <h3>Обратная связь</h3>
+    <table>
+        <tr>
+            <td>
+                Имя: <?= $_POST['name'] ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Телефон: <?= $_POST['tel'] ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Email: <?= $_POST['email'] ?>
+            </td>
+        </tr>
+    </table>
+    <?
+    $html = ob_get_contents();
+    ob_end_clean();
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = $mail::CHARSET_UTF8;
+
+    try {
+        //Recipients
+        $mail->setFrom('info@suntelpro.ru', 'Suntelpro');
+        $mail->addAddress('mikhail-seregin@yandex.ru', 'БПЦ');
+
+        //Content
+        $mail->isHTML(true);
+        $mail->Subject = 'Обратная связь';
+        $mail->Body = $html;
+
+        $result = $mail->send();
+        return $result;
+    } catch (Exception $e) {
+        die(print_r($e));
+        return false;
+    }
+
 }
+
+echo json_encode(array(
+    'status' => sendMail()
+));
+exit();
+
